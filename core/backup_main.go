91a7fae011a8e7858f7e2fe7f029100b694d9a8c
Kinsomnia <118033324+Kinsomnia@users.noreply.github.com>
2023-07-20 18:29:20 +0800

    Create backup_main.go

---
+++ core/backup_main.go
@@ -0,0 +1,82 @@
+package core
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func BackupMain() {
+	var path string
+	// 获取当前时间
+	now := time.Now().Format("2006-01-02")
+
+	// 创建备份文件
+	filename := "backup_" + now + ".tar.gz"
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// 创建Gzip压缩器
+	gzipWriter := gzip.NewWriter(file)
+	defer gzipWriter.Close()
+
+	// 创建Tar打包器
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer tarWriter.Close()
+
+	// 遍历目录并添加到备份文件中
+	fmt.Println("Please input the source file:")
+	fmt.Scanln(&path) // 要备份的目录
+	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 跳过备份文件和备份目录
+		if path == filename || info.IsDir() && info.Name() == "backup" {
+			return nil
+		}
+
+		// 创建文件头信息
+		header, err := tar.FileInfoHeader(info, info.Name())
+		if err != nil {
+			return err
+		}
+		header.Name = path
+
+		// 写入文件头信息
+		err = tarWriter.WriteHeader(header)
+		if err != nil {
+			return err
+		}
+
+		// 如果是普通文件，则写入文件内容
+		if info.Mode().IsRegular() {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarWriter, file)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// 备份完成
+	println("Backup completed!")
+}
