e229b4e308f32dd678a5ce89f630cb5423e8b215
Kinsomnia <118033324+Kinsomnia@users.noreply.github.com>
2023-07-21 08:39:11 +0800

    Update password.go

--- core/password.go
+++ core/password.go
@@ -7,8 +7,7 @@ import (
 	"time"
 )
 
-func password() int {
-	var passwd, name string
+func Password(passwd string, name string) int {
 	// 连接数据库
 	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test?parseTime=true")
 	if err != nil {
@@ -21,19 +20,13 @@ func password() int {
 	var password string
 	var attempts int
 	var lastAttempt time.Time
-	fmt.Println("Please input your username:")
-	fmt.Scan(&name)
 	err = db.QueryRow("SELECT username, password, attempts, last_attempt FROM user WHERE username=?", name).Scan(&username, &password, &attempts, &lastAttempt)
 	if err != nil {
 		fmt.Println("用户不存在")
 	}
 
-	// 检查密码和密码次数
-	fmt.Println("Please input your password:")
-	fmt.Scan(&passwd)
 	if password == passwd && attempts < 3 {
 		fmt.Printf("你已经成功登录账户%s\n", name)
-		return 1
 		// 在这里可以进行其他操作，如获取用户信息等
 
 		// 重置密码次数和最后一次尝试时间
@@ -41,6 +34,7 @@ func password() int {
 		if err != nil {
 			panic(err.Error())
 		}
+		return 1
 	} else if password != passwd && attempts < 3 {
 		fmt.Println("密码错误！账号", name, "还剩", 2-attempts, "次机会。")
 		// 更新密码次数和最后一次尝试时间
@@ -54,3 +48,4 @@ func password() int {
 	}
 	return 3
 }
+
